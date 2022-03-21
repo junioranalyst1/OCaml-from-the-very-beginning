2ae0d9f50c4ca1225b8a45171df75ae0541b4d9b
junioranalyst1 <75017139+junioranalyst1@users.noreply.github.com>
2022-03-21 14:19:08 +0000

    edit problems chapter 3

--- Chapter 3/Q5.ml
+++ Chapter 3/Q5.ml
@@ -1,8 +1,5 @@
-# it evaluates to an integer (- : int = 5)
-# We can rewrite the expression as such:
-
 match 1 + 1 with
   2 ->
-  (match 2 + 2 with
-   3 -> 4
-  | 4 -> 5);;
+    (match 2 + 2 with
+    3 -> 4
+    | 4 -> 5);;
